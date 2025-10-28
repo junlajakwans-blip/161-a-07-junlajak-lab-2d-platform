64d7601119a181b7911919be27c2c9859ecf2913
JUNLAJAK_WANSUKSRI <junlajak.wans@bumail.net>
2025-10-28 09:35:26 +0700

    SpawnPoint

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public int damage;
+    public abstract void Move();
+    public abstract void OnHitWith(Character character);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
